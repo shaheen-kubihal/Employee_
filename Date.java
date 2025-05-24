b7a65a3569702aa529c39d2f5f2d1aa121fe243b
shaheen-kubihal <shaheenkubihal@gmail>
2025-05-24 10:21:37 +0530

    save

--- Date.java
+++ Date.java
@@ -14,7 +14,7 @@ class student {
         System.out.println("Course: " + course);
     }
 }
-public class Date{
+public class Pgm2{
     public static void main(String[] args) {
         student stud = new student("Alice", 101, "Computer Science");
         stud.display();
